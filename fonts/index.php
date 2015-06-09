f631a609b32392c80e94d67102384209d3680d95
mi <mhiezely@hotmail.fr>
2015-06-09 17:13:49 +0200

    les fonts; les pictos et la feuille de style

---
+++ fonts/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel ="stylesheet" href="style.css"/>
+		<title>Ma petite agence</title>
+	</head>
+	<body>
+		<header>
+			<ul>
+				<li>Accueil</li>
+				<li>Présentation</li>
+				<li>Travaillons ensemble</li>
+				<li>Réalisations</li>
+				<li>Contact</li>
+			</ul>
+				
+		</header>
+		<!-- conteneur des sections -->
+		<div class="content">
+		<!--section 1-->	
+			<section id="sect1">
+				<!--logo + flèche vers le bas-->
+			</section>
+			
+		<!--section 2-->	
+			<section id="sect2">
+			<h1>Pourquoi votre présence sur internet est essentielle?</h1>
+			<h2>Vous êtes une entreprise</h2>
+			<p><!--texte pour les auto entrepreneurs....--></p>
+			<h2>Pour votre communauté</h2>
+			<p><!--texte pour blogeurs--></p>
+			<h2>Pour votre carrière</h2>
+			<p><!--texte pour cv en ligne-->
+			</section>
+			
+		<!--section 3-->	
+			<section id="sect3">
+			<h1>Qui est Ma Petite Agence Web?</h1>
+			<p><!--txte de presentation-->
+			</section>
+		
+		<!--section 4-->	
+			<section id="sect4">
+			<h1>Ce qu'on peut faire enssemble</h1>
+			<h2>Des site évolutifs</h2>
+			<h2>Vous êtes autonome</h2>
+			<h2>On s'occupe de tout!</h2>
+			</section>
+			
+		<!--section 5-->	
+			<section id="sect5">
+			<h1>Quelques réalisations</h1>
+			</section>
+		
+		<!--section 6-->	
+			<section id="sect6">
+			<h1>Prenons contact</h1>
+			<!--ici un formulaire de contact-->
+			</section>
+		</div>
+		<footer>
+			<!--mentions légales, réseaux sociaux, plan du site-->
+		</footer>
+	</body>
+</html>
+		
---
+++ fonts/readme.txt
@@ -0,0 +1,10 @@
+Création d'un site web one page pour une mini agence web.
+
+Mini agence mais maxi efficacité!
+
+Site crée dans le cadre d'une formation. 
+
+Souhait +++ : respecter les bonnes pratiques du W3C
+
+Help me <3
+
